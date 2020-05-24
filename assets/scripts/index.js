9788700901c34f27438624dc61dcc848908d8345
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-05-24 14:22:14 +0100

    add experimental switch

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -142,6 +142,25 @@ class ContentsTable {
   }
 }
 
+class ExperimentalSwitch {
+  static create() {
+    return new ExperimentalSwitch();
+  }
+
+  constructor() {
+    this.count = 0;
+    const themeSwitch = document.querySelector('#themeSwitch');
+    themeSwitch.addEventListener('click', () => {
+      this.count++;
+
+      if (this.count === 4) {
+        document.querySelector('body').classList.toggle('experimental-on');
+        this.count = 0;
+      }
+    });
+  }
+}
+
 class App {
   static create() {
     return new App();
@@ -152,6 +171,7 @@ class App {
     ScrollSpy.create();
     CodeSnippets.create();
     ContentsTable.create();
+    ExperimentalSwitch.create();
   }
 }
 
