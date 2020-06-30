4bae5423d7506f6358c5c23475d17a74f18c6ee5
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-07-01 00:18:13 +0100

    add text for no found articles

--- assets/scripts/site-search.js
+++ assets/scripts/site-search.js
@@ -39,8 +39,18 @@ class SiteSearch {
   }
 
   updateDom(matches) {
-    const articlesList = document.querySelector('.articles-list');
+    const articlesList = document.querySelector('.articles-list'),
+      noArticlesText = document.querySelector('.articles-noArticles');
+
     articlesList.innerHTML = '';
+
+    console.log(matches.length);
+
+    if (!matches.length) {
+      noArticlesText.classList.add('show');
+    } else {
+      noArticlesText.classList.remove('show');
+    }
 
     matches.forEach((match) => {
       articlesList.appendChild(match);
